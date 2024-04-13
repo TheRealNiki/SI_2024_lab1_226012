8aceedd5d4c25009b25c6679e3f897587d9dda62
TheRealNiki <golubovskinikola@gmail.com>
2024-04-14 00:37:27 +0200

    First push commit

--- doc2.java
+++ doc2.java
@@ -1,16 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class SILab1_1 {
-    public static List<String> filterOddLengthStrings(List<String> list) {
-        List<String> result = new ArrayList<>();
-        for (String el : list) {
-            if (el.length() % 2 == 0) {
-                result.add(el);
-            }
+public static List<String> filterOddLengthStrings(List<String> list) {
+    List<String> result = new ArrayList<>();
+    for (String el : list) {
+        if (el.length() % 2 != 0) { // Изменето условот за филтрирање на стринговите со непарна должина
+            result.add(el);
         }
-        return result;
     }
+    
+    return result;
+}
 
+public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    list.add("one");
+    list.add("two");
+    list.add("three");
+    list.add("four");
+    list.add("five");
 
+    System.out.println(filterOddLengthStrings(list)); // Печатење на резултатот
 }
