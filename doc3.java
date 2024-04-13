4ffd6574d09dcbd177b9843a95170c3dd621c54b
TheRealNiki <golubovskinikola@gmail.com>
2024-04-13 23:09:08 +0200

    Call method makeBigStringFrom in main class

--- doc3.java
+++ doc3.java
@@ -7,3 +7,17 @@ private static String makeBigStringFrom(List<String> list, int minLength) {
     }
     return sb.toString();
 }
+
+public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+
+        
+
+        // Повик на методот makeBigStringFrom и печатење на резултатот
+        System.out.println(SILab1_2.makeBigStringFrom(list, 3)); // Пример за минимална должина на стринговите 3
+}
